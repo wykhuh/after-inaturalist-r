0c8cac07667434f97ca0ce904a6c15c76df0c288
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-12-18 04:01:42 -0600

    update script to convert qmd to R scripts

--- scripts/create_script_files.R
+++ scripts/create_script_files.R
@@ -1,24 +1,23 @@
+processFile = function(filepath) {
+  original_text  <- readLines(filepath)
+  print(original_text)
+
+  original_text |> gsub(pattern = "^#\\| message: false", replace = "", perl = FALSE) |>
+    gsub(pattern = "^#\\| warning: false", replace = "", perl = FALSE) |>
+    gsub(pattern = "^#\\| error: false", replace = "", perl = FALSE) |>
+    gsub(pattern = "^#\\| cache: true", replace = "", perl = FALSE) |>
+    writeLines(con=filepath)
+
+}
+
 input_file <- "lessons/working-with-data.qmd"
 output_file <- "scripts/working-with-data.R"
 
+input_file <- "lessons/census.qmd"
+output_file <- "scripts/census.R"
 
 
 
-processFile = function(filepath) {
-  con = file(filepath, "r")
-  while ( TRUE ) {
-    line = readLines(con, n = 1)
-    if ( length(line) == 0 ) {
-      break
-    }
-    clean_line = gsub("#'", '',line)
-    print(clean_line)
-  }
+knitr::purl(input = input_file, output = output_file,documentation = 1)
 
-  close(con)
-}
-
-
-knitr::purl(input = input_file, output = output_file,documentation = 0)
-
-#processFile(output_file)
+processFile(output_file)
